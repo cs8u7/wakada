51044a72b0b2cbeee9f628de9d363b5138c95514
cs8u7 <tranductuan247@gmail.com>
2023-03-07 10:22:54 +0700

    add b2_os and js

---
+++ JavalorantScript/index.js
@@ -0,0 +1,180 @@
+// TODO Brocode
+//* vid1
+// console.log("sus");
+// window.alert("CHECK BACK!")
+
+//* vid2 - variable
+// let first_name = "8u7"
+// document.getElementById("p1").innerHTML = "Hello " + first_name
+// console.log(first_name)
+
+//*vid3 - arithmetic
+// let number = 20
+// number = number + 1
+// number = number - 1
+// number = number * 2
+// number = number / 3
+// number = number % 5
+// console.log(number)
+
+//* vid4 - accept user unit
+//! Window prompt
+// let username = window.prompt("Who are you")
+// console.log(username)
+//! HTML
+// let user_name
+// document.getElementById("myButton").onclick = function(){ 
+//    user_name = document.getElementById("myText").value
+//    console.log(user_name)
+//    document.getElementById("myLabel").innerHTML = user_name
+// }
+
+//* vid5 - conversion of var types
+// let age = window.prompt("How old are you?: ")
+// console.log(typeof age)
+// age = Number(age)
+// console.log(typeof age)
+// age += 1
+// console.log("You are",age,"years old!")
+
+// let x,y,z
+// x = Number("3.14")
+// console.log(x,typeof x)
+// y = String(3.14)
+// console.log(y,typeof y)
+// z = Boolean("")
+// console.log(z,typeof z)
+// z = Boolean("simp")
+// console.log(z,typeof z)
+
+//*8 Math 
+// let x = 3.14
+// let y = 5
+// let z = 9
+// x = Math.round(x)
+// x = Math.ceil(x)
+// x = Math.floor(x)
+// x = Math.pow(x,2)
+// x = Math.sqrt(x)
+// x = Math.abs(x)
+// let maximum = Math.max(x,y,z)
+// let minimum = Math.min(x,y,z)
+// console.log(x)
+
+//*9
+// let a,b,c
+// a = window.prompt("Enter a side")
+// b = window.prompt("Enter b side")
+// a = Number(a)
+// b = Number(b)
+// c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+// console.log("Side c is",c)
+
+// let a,b,c
+// c = Number(c)
+// document.getElementById("Button").onclick = function(){
+   
+//    a = document.getElementById("aText").value
+//    a = Number(a)
+//    b = document.getElementById("bText").value
+//    b = Number(b)
+//    c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+//    console.log(c)
+//    document.getElementById("c_name").innerHTML = "c is " + c
+// }
+
+//*10 - counting
+// let count = 0
+// document.getElementById("downButton").onclick = function(){
+//    count -= 1
+//    document.getElementById("countLabel").innerHTML = count
+// }
+// document.getElementById("upButton").onclick = function(){
+//    count += 1
+//    document.getElementById("countLabel").innerHTML = count
+// }
+// document.getElementById("resetButton").onclick = function(){
+//    count = 0
+//    document.getElementById("countLabel").innerHTML = count
+// }
+
+//*11 - random value
+// let x = Math.floor(Math.random() * 6) + 1
+// console.log(x)
+
+//*12 - strings method
+// let phone_number = "123-456-789"
+// let user_name = "  r1c3p0p  "
+// console.log(user_name.length)
+// console.log(user_name.charAt(2))
+// console.log(user_name.indexOf("0"))
+// console.log(user_name.trim())
+// phone_number = phone_number.replaceAll("-", "/")
+// console.log(phone_number)
+
+//*14 - chaining
+// let user_name = "Bruh"
+// let letter = user_name.charAt(0).toUpperCase().trim()
+// console.log(letter)
+
+//*15 - if/else
+//same c language
+// let state = true
+// if(state){
+//    console.log("You are online")
+// }
+// else{
+//    console.log("You are offline")
+// }
+
+//*16 - check property
+// document.getElementById("myButton").onclick = function(){
+//    if(document.getElementById("myCheckbox").checked){
+//       console.log("You are sus")
+//    }
+//    else{
+//       console.log("You are not sus")
+//    }
+// }
+
+//*17 - switch case
+// let grade = "B"
+// switch(grade){
+//    case "A":
+//       console.log("A")
+//       break
+//    case "B":
+//       console.log("B")
+//       break
+//    case "C":
+//       console.log("C")
+//       break
+//    default:
+//       console.log(grade, "is not a grade")
+// }
+
+// let grade = 86
+// switch(true){
+//    case grade >= 90:
+//       console.log("S")
+//       break
+//    case grade >= 80:
+//       console.log("A")
+//       break
+//    case grade >= 70:
+//       console.log("B")
+//       break
+//    default:
+//       console.log(grade, "is not a grade")
+// }
+
+//*18 while loop
+let username = ""
+while(username == ""){
+   console.log("a")
+}
+
+
+// TODO Book
+// let x = 2 + 3 + "5"
+// console.log(x)
